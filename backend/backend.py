5569f752d3df7a3de525c0e8e89ed68fb251e430
AljazR <aljoz.rakovec@gmail.com>
2024-04-20 23:55:57 +0200

    Basic backend

--- backend/backend.py
+++ backend/backend.py
@@ -1,1 +1,37 @@
-print("Hello world!")
+import os
+from flask import Flask, request, jsonify
+from twilio.rest import Client
+from gpt import get_message
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return """
+    <h1>Juicer is running \U0001F34B \U00002192 \U0001F9C3</h1>
+    <p>Send a POST request to /send_sms with a JSON body containing a message and phone_number to send an SMS</p>
+    """
+
+
+@app.route('/send_sms', methods=['POST'])
+def send_sms():
+
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+
+    # text = get_message(request.json['message'])
+    text = "Dj mi dnar!"
+
+    message = client.messages.create(
+        body=text,
+        from_=os.environ['TWILIO_PHONE_NUMBER'],
+        to=request.json['phone_number']
+    )
+
+    return jsonify({'message': text, 'sid': message.sid})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend/gpt.py
@@ -0,0 +1,64 @@
+import os
+import openai
+
+TOKEN = os.environ["API_TOKEN"]
+BASE_URL = "https://openai-proxy.sellestial.com/api"
+
+
+def get_message(params):
+    '''
+    params:
+    {
+        "sender": "Rihard Šmid",
+        "reason": "Concert tickets",
+        "agressivnes": 5,
+        "mafiozo": True,
+        "citations": True,
+        "stay_friends": False
+    }
+    '''
+
+    TOKEN = os.environ["OPENAI_API_TOKEN"]
+    BASE_URL = "https://openai-proxy.sellestial.com/api"
+
+    client = openai.OpenAI(api_key=TOKEN, base_url=BASE_URL)
+    model = "gpt-4-turbo"
+    # model = "gpt-3.5-turbo"
+
+    messages = [
+        {
+            "role": "system",
+                    "content": "As a good post writer, your job is to write tweets that express protest on a given topic. \
+                    When writing these tweets, it is important to use the same language as the topic of protest is written in.\
+                    Also at the end of the post mention relavant politicians, that are responsible for the topic of protest. They have to be from the country of language of the post.",
+        },
+        {"role": "user", "content": "Davki kmetov"},
+        {
+            "role": "assistant",
+                    "content": "Obdavčevanje naših pridnih kmetov je sramota! Slovenija se mora zavedati, da je naša kmetijska industrija hrbtenica našega gospodarstva. \
+                                                Nehajte kaznovati tiste, ki nam zagotavljajo hrano, in začnite jih podpirati! #StopDavkomNaKmete #SlovenskaKmetijstvo #PodpriteKmete @MKGP_RS @vladaRS",
+        },
+        {"role": "user", "content": "Davki kmetov"},
+        {
+            "role": "assistant",
+                    "content": "Obdavčevanje naših pridnih kmetov je sramota! Slovenija se mora zavedati, da je naša kmetijska industrija hrbtenica našega gospodarstva. \
+                                                Nehajte kaznovati tiste, ki nam zagotavljajo hrano, in začnite jih podpirati! #StopDavkomNaKmete #SlovenskaKmetijstvo #PodpriteKmete @MKGP_RS @vladaRS",
+        },
+        {"role": "user", "content": "Davki kmetov"},
+        {
+            "role": "assistant",
+                    "content": "Obdavčevanje naših pridnih kmetov je sramota! Slovenija se mora zavedati, da je naša kmetijska industrija hrbtenica našega gospodarstva. \
+                                                Nehajte kaznovati tiste, ki nam zagotavljajo hrano, in začnite jih podpirati! #StopDavkomNaKmete #SlovenskaKmetijstvo #PodpriteKmete @MKGP_RS @vladaRS",
+        },
+        {"role": "user", "content": f"{params}"},
+    ]
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+    )
+
+    if response.ok:
+        print(response.json()["choices"][0]["message"]["content"])
+
+    return response.json()["choices"][0]["message"]["content"]
